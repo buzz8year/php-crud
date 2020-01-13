7bbedb88e86df37a76f7b3b93e2dda103552a451
cocostru <dk.pochtamp@gmail.com>
2020-01-14 03:27:08 +0800

    lazy

--- src/helpers/Strings.php
+++ src/helpers/Strings.php
@@ -6,11 +6,6 @@ class Strings
 {
 	public static function prepareOrderBy(string $orderBy, string $default) : ?string
 	{
-        if (empty($orderBy)) 
-        {
-            $orderBy = $default;
-        }
-
         // EXPLAIN: ...
         if (strpos($orderBy, '-') === false) 
         {
