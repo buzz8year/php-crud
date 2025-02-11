f706caf4a2aa821783405818f1eda0de85cdcaeb
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 03:51:59 +0400

    Update Strings.php

--- src/helpers/Strings.php
+++ src/helpers/Strings.php
@@ -6,19 +6,15 @@ class Strings
 {
 	public static function prepareOrderBy(string $orderBy, string $default) : ?string
 	{
-        // EXPLAIN: ...
-        if (strpos($orderBy, '-') === false) 
-        {
-            $orderBy .= ' ASC';
-        }
-        else 
-        {
-            $orderBy = trim($orderBy, '-');
-            $orderBy .= ' DESC';
-        }	
-
-        return $orderBy;
+	        if (strpos($orderBy, '-') === false) 
+	        {
+	            $orderBy .= ' ASC';
+	        }
+	        else {
+	            $orderBy = trim($orderBy, '-');
+	            $orderBy .= ' DESC';
+	        }	
+	        return $orderBy;
 	}
-
 	
 }
