7b11e7d1661bf6c10d440dbe9bcf00d48abbfdc2
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 04:51:09 +0400

    Update Task.php

--- src/models/Task.php
+++ src/models/Task.php
@@ -26,9 +26,7 @@ class Task
     public static function populateSlice(string $orderBy, int $limit, int $offset, \PDO $pdo = null) : void
     {
         if (!isset($pdo)) 
-        {
             $pdo = PDOFactory::readInstance();
-        }
 
         // WARNING: As to hardcoded $orderBy, - table and column /
         // names CANNOT be replaced by parameters, in PDO 
@@ -41,22 +39,17 @@ class Task
             OFFSET :slice_offset
         ");
 
-        $handle->execute(
-            array(
-                'slice_limit' => $limit,
-                'slice_offset' => $offset,
-            )
-        );
+        $handle->execute(array(
+            'slice_limit' => $limit,
+            'slice_offset' => $offset,
+        ));
 
         while ($object = $handle->fetchObject('\models\Task')) 
-        {
             self::$slice[(int)$object->getId()] = $object;
-        }
-
     }
 
 
-    public static function getSlice(string $orderBy = null, int $limit, int $offset)
+    public static function getSlice(string $orderBy = null, int $limit, int $offset) : array
     {
         self::populateSlice($orderBy, $limit, $offset);
 
@@ -67,9 +60,7 @@ class Task
     public static function get(int $id, \PDO $pdo = null) : Task
     {
         if (!isset($pdo)) 
-        {
             $pdo = PDOFactory::readInstance();
-        }
 
         $handle = $pdo->prepare('
             SELECT id, user_email, name, text, status, edited 
@@ -77,21 +68,15 @@ class Task
             WHERE id = :id
         ');
 
-        $handle->execute(
-            array(
-                ':id' => $id
-            )
-        );
+        $handle->execute(array(
+            ':id' => $id
+        ));
 
 
         if ($handle->rowCount() !== 1) 
-        {
             self::$slice[(int)$id] = new self;
-        } 
-        else 
-        {
-            self::$slice[(int)$id] = $handle->fetchObject('\models\Task');
-        }
+        
+        else self::$slice[(int)$id] = $handle->fetchObject('\models\Task');
 
 
         return self::$slice[(int)$id];
@@ -102,9 +87,7 @@ class Task
     public static function countAll(\PDO $pdo = null)
     {
         if (!isset($pdo)) 
-        {
             $pdo = PDOFactory::readInstance();
-        }
 
         $handle = $pdo->prepare('SELECT count(*) FROM `task`');
         $handle->execute();
@@ -116,53 +99,29 @@ class Task
    public function create(array $data, \PDO $pdo = null) : bool
     {
         if (empty($data)) 
-        {
             return false;
-        }
 
-        else
-        {
-            if (empty($pdo)) 
-            {
-                $pdo = PDOFactory::readInstance();
-            }
+        if (empty($pdo)) 
+            $pdo = PDOFactory::readInstance();
 
-            $handle = $pdo->prepare('
-                INSERT INTO `task` 
-                (
-                    user_email,
-                    status,
-                    edited,
-                    name,
-                    text
-                ) 
-                VALUES 
-                (
-                    :user_email,
-                    :status,
-                    :edited,
-                    :name,
-                    :text
-                ) 
-            ');
+        $handle = $pdo->prepare("
+            INSERT INTO `task` (user_email, status, edited, name, text) 
+            VALUES (:user_email, :status, :edited, :name, :text) 
+        ");
 
-            // NOTE: As to :text, - htmlentities() or none of its siblings 
-            // is not used to write to text value, for the convience of parsing 
-            // xss-potential injection attempts and trials, chiefly on big portions of data.
+        // NOTE: As to :text, - htmlentities() or none of its siblings 
+        // is not used to write to text value, for the convience of parsing 
+        // xss-potential injection attempts and trials, chiefly on big portions of data.
 
-            // NOTE: Instead, htmlspecialchars() or one of its siblings 
-            // is obligatory on rendering xss-potential data in client browser.
-            return $handle->execute(
-                array(
-                    ':edited'       => 0,
-                    ':status'       => 1,
-                    ':name'         => $data['task_name'],
-                    ':user_email'   => $data['task_usermail'],
-                    ':text'         => $data['task_text'],
-                )
-            );
-
-        }
+        // NOTE: Instead, htmlspecialchars() or one of its siblings 
+        // is obligatory on rendering xss-potential data in client browser.
+        return $handle->execute(array(
+            ':edited'       => 0,
+            ':status'       => 1,
+            ':name'         => $data['task_name'],
+            ':user_email'   => $data['task_usermail'],
+            ':text'         => $data['task_text'],
+        ));
 
     }
 
@@ -170,55 +129,40 @@ class Task
     public function update(array $data, \PDO $pdo = null) : bool
     {
         if (empty($data)) 
-        {
             return false;
-        }
 
-        else
-        {
-            if (empty($pdo)) 
-            {
-                $pdo = PDOFactory::readInstance();
-            }
+        if (empty($pdo)) 
+            $pdo = PDOFactory::readInstance();
 
-            $handle = $pdo->prepare('
-                UPDATE `task` SET
-                    user_email = :user_email,
-                    status = :status,
-                    edited = :edited,
-                    name = :name,
-                    text = :text
-                WHERE id = :id
-            ');
+        $handle = $pdo->prepare('
+            UPDATE `task` SET
+                user_email = :user_email,
+                status = :status,
+                edited = :edited,
+                name = :name,
+                text = :text
+            WHERE id = :id
+        ');
 
-            if ( empty( $data['task_text'] ) ) 
-            {
-                $edited = $this->getEdited();
-            }
-            else
-            {
-                $edited = $data['task_text'] === $this->getText() ? 0 : 1;
-            }
+        if (empty($data['task_text'])) 
+            $edited = $this->getEdited();
+        else $edited = $data['task_text'] === $this->getText() ? 0 : 1;
 
-            // NOTE: As to :text, - htmlentities() or none of its siblings 
-            // is not used to write to text value, for the convience of parsing 
-            // xss-potential injection attempts and trials, chiefly on big portions of data.
+        // NOTE: As to :text, - htmlentities() or none of its siblings 
+        // is not used to write to text value, for the convience of parsing 
+        // xss-potential injection attempts and trials, chiefly on big portions of data.
 
-            // NOTE: Instead, htmlspecialchars() or one of its siblings 
-            // is obligatory on rendering xss-potential data in client browser.
+        // NOTE: Instead, htmlspecialchars() or one of its siblings 
+        // is obligatory on rendering xss-potential data in client browser.
 
-            return $handle->execute(
-                array(
-                    ':id'           => $data['task_id'] ?? $this->getId(),
-                    ':name'         => $data['task_name'] ?? $this->getName(),
-                    ':user_email'   => $data['task_usermail'] ?? $this->getUserEmail(),
-                    ':status'       => $data['task_status'] ?? $this->getStatus(),
-                    ':text'         => $data['task_text'] ?? $this->getText(),
-                    ':edited'       => $edited,
-                )
-            );
-
-        }
+        return $handle->execute(array(
+            ':id'           => $data['task_id'] ?? $this->getId(),
+            ':name'         => $data['task_name'] ?? $this->getName(),
+            ':user_email'   => $data['task_usermail'] ?? $this->getUserEmail(),
+            ':status'       => $data['task_status'] ?? $this->getStatus(),
+            ':text'         => $data['task_text'] ?? $this->getText(),
+            ':edited'       => $edited,
+        ));
 
     }
 
