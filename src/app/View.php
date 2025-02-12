de63e625cef83ce8bcae8d229a3c502a3b167adb
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 04:21:33 +0400

    Update View.php

--- src/app/View.php
+++ src/app/View.php
@@ -4,18 +4,13 @@ namespace app;
 
 class View 
 {
-    // EXPLAIN: ...
     protected $template = null;
 
-    
-    // EXPLAIN: ...
     public function __construct(string $template) 
     {
         $this->template = $template;
     }
 
-    
-    // EXPLAIN: ...
     public function render(array $data) 
     {
         include('../src/views/' . $this->template . '.php');
@@ -24,5 +19,4 @@ class View
         ob_get_clean();
     }
     
-
 }
