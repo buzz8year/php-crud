716fb05978e933e66ce48b3201c5fd6fa83ca7da
buzz8year <dk.pochtamp@gmail.com>
2025-02-12 04:20:13 +0400

    Update PDOFactory.php

--- src/db/PDOFactory.php
+++ src/db/PDOFactory.php
@@ -7,30 +7,30 @@ class PDOFactory
     protected static $pdo_written_instance;
     protected static $pdo_red_instance;
 
-    public static function writeInstance()
-    {
-        // EXPLAIN: Condition will be required on future unit testing
-        if (!isset(self::$pdo_written_instance)) 
-        {
-            self::$pdo_written_instance = new \PDO(
-                sprintf("mysql:host=%s;port=%u;dbname=%s;charset=UTF8", MYSQL_HOST, MYSQL_PORT, MYSQL_DB),
-                MYSQL_USER,
-                MYSQL_PASS,
-                array(
-                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'set names utf8',
-                    \PDO::ATTR_EMULATE_PREPARES => false
-                )
-            );
-
-            // EXPLAIN: ...
-            self::$pdo_written_instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        }
-
-        return self::$pdo_written_instance;
-    }
-
     public static function readInstance()
     {
         return self::writeInstance();
     }
+
+    public static function writeInstance()
+    {
+        // NOTE: Condition required for testing
+        if (!isset(self::$pdo_written_instance)) 
+            self::instantiateMySQL();
+        
+        return self::$pdo_written_instance;
+    }
+
+    public static function instantiateMySQL()
+    {
+        $dsn = sprintf("mysql:host=%s;port=%u;dbname=%s;charset=UTF8", MYSQL_HOST, MYSQL_PORT, MYSQL_DB);
+        
+        self::$pdo_written_instance = new \PDO($dsn, MYSQL_USER, MYSQL_PASS, array(
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'set names utf8',
+            \PDO::ATTR_EMULATE_PREPARES => false
+        ));
+        
+        self::$pdo_written_instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+    
 }
